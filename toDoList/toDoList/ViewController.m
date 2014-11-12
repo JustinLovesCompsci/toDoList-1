943fdb6cacab0ad175630929c0600272cb1f2724
JustinLovesCompsci <justinzihaozhang@gmail.com>
2014-11-11 20:29:47 -0500

    update

--- toDoList/toDoList/ViewController.m
+++ toDoList/toDoList/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "DLDataModel.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITableView datasource
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [[[DLDataModel sharedInstance] currentList] count];
 }
 
 @end
